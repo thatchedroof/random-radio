f3c1d8af0c986ee17462699bc0783f0165ead786
thatchedroof <tnert.wire100@gmail.com>
2025-10-15 13:33:03 -0700

    Update Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'path';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		alias: {
-			"random-radio": resolve('./random-radio')
-		}
-	}
+  kit: {
+    adapter: adapter({
+      fallback: "404.html",
+    }),
+    alias: {
+      "random-radio": resolve("./random-radio"),
+    },
+    paths: {
+      base: process.argv.includes("dev") ? "" : "/random-radio",
+    },
+  },
 };
 
 export default config;
